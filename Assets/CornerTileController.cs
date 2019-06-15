5187be93a3ac4ffaf969120ec06c10f2d3ca2818
KatGutierrez <katrina.gutierrez@ludus-vr.com>
2019-06-15 23:49:42 +0200

    Added corners & walls

---
+++ Assets/CornerTileController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CornerStates
+{
+    TopL, TopR, BottomL, BottomR
+}
+
+public class CornerTileController : MonoBehaviour
+{
+    [SerializeField]
+    public CornerStates CurrentState;
+    [SerializeField]
+    public GameObject Model;
+
+    [SerializeField]
+    public GameObject TopLSprite;
+
+    [SerializeField]
+    public GameObject TopRSprite;
+
+    [SerializeField]
+    public GameObject BottomLSprite;
+
+    [SerializeField]
+    public GameObject BottomRSprite;
+
+    private Vector3 currentAngle;
+    private Vector3 targetAngle = new Vector3(0, 0, 0);
+
+    private void Start()
+    {
+        ChangeState(CurrentState);
+    }
+
+    void Update()
+    {
+        currentAngle = new Vector3(
+             Mathf.LerpAngle(currentAngle.x, targetAngle.x, (1 * Time.deltaTime)),
+             Mathf.LerpAngle(currentAngle.y, targetAngle.y, (1 * Time.deltaTime)),
+             Mathf.LerpAngle(currentAngle.z, targetAngle.z, (1 * Time.deltaTime)));
+
+        Model.transform.eulerAngles = currentAngle;
+    }
+
+    public void ChangeState(CornerStates newState)
+    {
+        switch (newState)
+        {
+            case CornerStates.TopL:
+
+                targetAngle = new Vector3(0, -90, 0);
+
+                break;
+
+            case CornerStates.TopR:
+
+                targetAngle = new Vector3(-90, 0, 0);
+
+                break;
+
+            case CornerStates.BottomL:
+
+                targetAngle = new Vector3(0, 180, -90);
+
+                break;
+
+            case CornerStates.BottomR:
+
+                targetAngle = new Vector3(90, -90, 0);
+
+                break;
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/Tile/AmbientTileController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientTileController : MonoBehaviour
+{
+    [SerializeField]
+    public TileStates CurrentState;
+    [SerializeField]
+    public GameObject Model;
+
+    [SerializeField]
+    public GameObject FireSprite;
+
+    [SerializeField]
+    public GameObject WaterSprite;
+
+    [SerializeField]
+    public GameObject NormalSprite;
+
+    [SerializeField]
+    public GameObject AirSprite;
+
+    private Vector3 currentAngle;
+    private Vector3 targetAngle = new Vector3(0, 0, 0);
+
+    private void Start()
+    {
+        ChangeState(CurrentState);
+    }
+
+    void Update()
+    {
+        currentAngle = new Vector3(
+             Mathf.LerpAngle(currentAngle.x, targetAngle.x, (1 * Time.deltaTime)),
+             Mathf.LerpAngle(currentAngle.y, targetAngle.y, (1 * Time.deltaTime)),
+             Mathf.LerpAngle(currentAngle.z, targetAngle.z, (1 * Time.deltaTime)));
+
+        Model.transform.eulerAngles = currentAngle;
+    }
+
+    public void ChangeState(TileStates newState)
+    {
+        switch (newState)
+        {
+            case TileStates.Normal:
+
+                targetAngle = new Vector3(0, 0, 0);
+
+                break;
+
+            case TileStates.Fire:
+
+                targetAngle = new Vector3(-90, -90, 0);
+
+                break;
+
+            case TileStates.Water:
+
+                targetAngle = new Vector3(0, -90,-90);
+
+                break;
+
+            case TileStates.Wind:
+
+                targetAngle = new Vector3(90, -90, 0);
+
+                break;
+        }
+
+
+
+    }
+}
+
