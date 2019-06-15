b18de81c5a557aecfe6903b4e4c0950a21cdd3a7
KatGutierrez <katrina.gutierrez@ludus-vr.com>
2019-06-15 21:05:46 +0200

    Working on sensors

---
+++ Assets/Scripts/Sensors/DamageSensor.cs
@@ -0,0 +1,44 @@
+﻿using Elementure.GameLogic.Agents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSensor : AgentSensor
+{
+
+    [SerializeField]
+    public int damage;
+
+
+    public override void Activate()
+    {
+        
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+
+        string AgentId = other.GetComponent<Agent>().Id;
+        GameObject Agent = other.gameObject;
+
+        switch (AgentId)
+        {
+            case "Player":
+
+                Agent.GetComponent<Agent>().Damage(damage);
+               // other.
+                Debug.Log("Eres el jugador");
+                break;
+
+            default:
+
+
+
+                break;
+        }
+
+    }
+
+
+}
