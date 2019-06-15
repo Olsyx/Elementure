202e40e0221130c849ed33d2ec0f65341514f21f
KatGutierrez <katrina.gutierrez@ludus-vr.com>
2019-06-15 09:26:22 +0200

    Init

---
+++ Assets/SensorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetType().Equals(Player))
+        {
+
+            Debug.Log("SoyPlayer");
+
+        }
+
+
+
+    }
+}
